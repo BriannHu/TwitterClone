44b2ec63494caab5d7667dba5e02f24f57f4f39d
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-06-29 21:46:21 -0400

    made and ran migrations

---
+++ tweets/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Tweet(models.Model):
+    # id = models.AutoField(primary_key=True)
+    # blank=true -> not required in django
+    # null=true -> not required in database
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
