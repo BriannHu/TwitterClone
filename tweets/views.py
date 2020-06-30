bcd86802d483e0865d508350341067402f8aa020
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-06-30 00:29:29 -0400

    added tweet_list_view in views.py

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 # own imports below
-from django.http import HttpResponse, Http404, JsonResponse
+from django.http import JsonResponse #,HttpResponse, Http404
 from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all() #query set
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     '''
