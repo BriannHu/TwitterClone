3177629d20e9de6cc06f16932d812c4ba87aead8
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-06-29 22:00:57 -0400

    added some dynamic url routing

--- tweets/models.py
+++ tweets/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
-    # blank=true -> not required in django
-    # null=true -> not required in database
+    # blank=True -> not required in django
+    # null=True -> not required in database
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
