6760e07ff58643337fb9e414824890b220643719
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-07-01 21:36:14 -0400

    installed django rest framework

--- tweet_app/settings.py
+++ tweet_app/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # own code below
+    # third-party
+    'rest-framework',
+    # internal
     'tweets',
 ]
 
--- tweets/views.py
+++ tweets/views.py
@@ -14,6 +14,9 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
+    '''
+    REST API CRUD view
+    '''
     user = request.user
     # checks if user is authenticated
     if not request.user.is_authenticated:
