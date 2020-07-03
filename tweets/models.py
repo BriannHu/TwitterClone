844489b7ad8cb754b3dfab4eff7986e999225474
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-07-02 23:48:04 -0400

    added TweetLike class to models.py

--- tweets/models.py
+++ tweets/models.py
@@ -4,16 +4,23 @@ import random
 
 User = settings.AUTH_USER_MODEL
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
     # blank=True -> not required in django
     # null=True -> not required in database
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     # foreign key -> many users can have many tweets
     # if owner is deleted, all their tweets are deleted
     # if want tweets to stay, set (User, null=True, on_delete=models.set_NULL) 
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-id']
