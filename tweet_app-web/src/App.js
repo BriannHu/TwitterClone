2180bbcdd129b028b6513dbbfaca3440a2274ca2
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-07-07 19:52:36 -0400

    added functional components in react

--- tweet_app-web/src/App.js
+++ tweet_app-web/src/App.js
@@ -1,13 +1,45 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+function loadTweets (callback) {
+  const xhr = new XMLHttpRequest()
+  const method = "GET" // as opposed to 'POST'
+  const url = "http://localhost:8000/api/tweets/"
+  const responseType = "json"
+  xhr.responseType = responseType
+  xhr.open(method, url)
+  xhr.onload = function () {
+    callback(xhr.response, xhr.status)
+  }
+  xhr.onerror = function (e) {
+    console.log(e)
+    callback({"message": "The request was an error"}, 400)
+  }
+  xhr.send()
+}
+
+function Tweet(props) {
+  const {tweet} = props
+  const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+  return <div className={className}>
+    <p>{tweet.id} - {tweet.content}</p>
+  </div>
+}
 
 function App() {
   const [tweets, setTweets] = useState([])
 
   useEffect(() => {
-    const tweetItems = [{"content": 123}, {"content": "Hello world"}]
-    setTweets(tweetItems)
+    const myCallBack = (response, status) => {
+      console.log(response, status)
+      if (status === 200) {
+        setTweets(response)
+      } else {
+        alert("There was an error.")
+      }
+    }
+    loadTweets(myCallBack)
   }, [])
 
   return (
@@ -17,11 +49,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          {tweets.map((tweet, index) => {
-            return <li>{tweet.content}</li>
+        <div>
+          {tweets.map((item, index) => {
+            return <Tweet tweet={item} className='mx-auto col-10 my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`}/>
           })}
-        </p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
