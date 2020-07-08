fe93f79f07c2f61c0c8a1f77c3dc8b42b7960db0
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-07-07 22:19:19 -0400

    added states in react

--- tweet_app-web/src/tweets/components.js
+++ tweet_app-web/src/tweets/components.js
@@ -21,17 +21,23 @@ export function TweetsList(props) {
 
 export function ActionBtn(props) {
     const {tweet, action} = props
+    const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
+    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
     const className = props.className ? props.className : 'btn btn-outline-primary btn-small'
     const actionDisplay = action.display ? action.display : 'Action'
-    let likes = tweet.likes
     const handleClick = (event) => {
         event.preventDefault()
         if (action.type === 'like') {
-            console.log(tweet.likes+1)
-            likes = tweet.likes + 1
+            if (userLike === true) {
+                setLikes(likes-1)
+                setUserLike(false)
+            } else {    
+                setLikes(likes+1)
+                setUserLike(true)
+            }
         }
     }
-    const display = action.type === 'like' ? `${tweet.likes} ${actionDisplay}` : actionDisplay
+    const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
     return <button className={className} onClick={handleClick}>{display}</button>
   }
   
