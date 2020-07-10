1e2c8bc22666ba4d37f56a86dd7e245086b9b053
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-07-09 23:22:54 -0400

    improved tweet action button

--- tweet_app-web/src/tweets/components.js
+++ tweet_app-web/src/tweets/components.js
@@ -1,5 +1,8 @@
 import React, {useEffect, useState} from 'react'
-import {apiTweetCreate, apiTweetList} from './lookup'
+import {
+  apiTweetAction,
+  apiTweetCreate,
+  apiTweetList} from './lookup'
 
 export function TweetsComponent(props) {
     const textAreaRef = React.createRef()
@@ -66,20 +69,20 @@ export function TweetsList(props) {
 export function ActionBtn(props) {
     const {tweet, action} = props
     const [likes, setLikes] = useState(tweet.likes ? tweet.likes : 0)
-    const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false)
+    // const [setUserLike] = useState(tweet.userLike === true ? true : false)
     const className = props.className ? props.className : 'btn btn-outline-primary btn-small'
     const actionDisplay = action.display ? action.display : 'Action'
+    
+    const handleActionBackendEvent = (response, status) => {
+      console.log(response, status)
+      if (status === 200) {
+        setLikes(response.likes)
+      }
+    }
     const handleClick = (event) => {
         event.preventDefault()
-        if (action.type === 'like') {
-            if (userLike === true) {
-                setLikes(likes-1)
-                setUserLike(false)
-            } else {    
-                setLikes(likes+1)
-                setUserLike(true)
-            }
-        }
+        apiTweetAction(tweet.id, action.type, handleActionBackendEvent)
+        
     }
     const display = action.type === 'like' ? `${likes} ${actionDisplay}` : actionDisplay
     return <button className={className} onClick={handleClick}>{display}</button>
