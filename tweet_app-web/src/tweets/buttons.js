575b871f30451990187472956387e016806f53fd
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-07-11 19:55:08 -0400

    added linking tweets and retweets

--- tweet_app-web/src/tweets/buttons.js
+++ tweet_app-web/src/tweets/buttons.js
@@ -4,7 +4,7 @@ import {apiTweetAction} from './lookup'
 export function ActionBtn(props) {
     const {tweet, action, didPerformAction} = props
     const likes = tweet.likes ? tweet.likes : 0
-    const className = props.className ? props.className : 'btn btn-outline-primary btn-small'
+    const className = props.className ? props.className : 'btn btn-outline-primary'
     const actionDisplay = action.display ? action.display : 'Action'
     
     const handleActionBackendEvent = (response, status) => {
