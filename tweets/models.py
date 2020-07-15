7272a5b4607339c2d8b69d94b43f331209dc41fa
Brainy505 <55677478+Brainy505@users.noreply.github.com>
2020-07-14 21:08:24 -0400

    added more tests for tweets

--- tweets/models.py
+++ tweets/models.py
@@ -17,7 +17,7 @@ class Tweet(models.Model):
     #   if owner is deleted, all their tweets are deleted
     #   if want tweets to stay, set (User, null=True, on_delete=models.set_NULL) 
     parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
-    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tweets')
     likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
